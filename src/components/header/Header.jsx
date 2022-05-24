8c0904d01ff91b7b9a9a6559d338a80eb7b9f61f
AAArmine <aaarmine.aghababyan@gmail.com>
2022-05-25 02:46:31 +0400

    adding some styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import chuckNorrisLogo from "../../assets/images/chuckNorrisLogo.png";
+import "./Header.scss";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <section className="header-sec w-100">
+      <div className="logo-container">
+        <img src={chuckNorrisLogo} alt="Chuk Norris" />
+      </div>
+    </section>
+  );
 };
